24c35d673976459659f51b189808e140a92c1d78
Working-aanas <workinganass@gmail.com>
2025-04-25 22:00:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,88 +5,91 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exit 1
+   echo "This script must be run as root" 
+   exit 1
 fi
 
 # Function to create user
 create_user() {
     echo "Please visit http://remotedesktop.google.com/headless and copy the command after Authentication"
     read -p "Paste the CRD SSH command here: " CRD
-    echo "Creating user and setting it up..."
-
-    username="eldas"
+    echo "Creating User and Setting it up"
+    username="disala"
     password="root"
     Pin="123456"
+    
+    useradd -m "$username"
+    adduser "$username" sudo
+    echo "$username:$password" | sudo chpasswd
+    sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 
-    if id "$username" &>/dev/null; then
-        echo "User $username already exists. Skipping user creation."
-    else
-        useradd -m "$username"
-        echo "$username:$password" | chpasswd
-        usermod -aG sudo "$username"
-        echo 'export PATH=$PATH:/home/'"$username"'/.local/bin' >> /home/"$username"/.bashrc
-        chown "$username":"$username" /home/"$username"/.bashrc
-        echo "User '$username' created and configured."
-    fi
+    # Add PATH update to .bashrc of the new user
+    echo 'export PATH=$PATH:/home/user/.local/bin' >> /home/"$username"/.bashrc
+    su - "$username" -c "source ~/.bashrc"
 
-    # Save CRD command for use later
-    echo "$CRD" > /home/"$username"/crd-command.sh
-    chmod +x /home/"$username"/crd-command.sh
-    chown "$username":"$username" /home/"$username"/crd-command.sh
+    echo "User '$username' created and configured."
 }
 
-# Function to setup storage
+# Extra storage setup
 setup_storage() {
-    echo "Setting up extra storage..."
     mkdir -p /storage
     chmod 777 /storage
     chown "$username":"$username" /storage
-
     mkdir -p /home/"$username"/storage
     mount --bind /storage /home/"$username"/storage
-    grep -qxF "/storage /home/$username/storage none bind 0 0" /etc/fstab || echo "/storage /home/$username/storage none bind 0 0" >> /etc/fstab
 }
 
-# Function to install CRD and dependencies
+# Function to install and configure RDP
 setup_rdp() {
-    echo "Installing necessary packages..."
-
-    apt update
-    apt install -y wget curl sudo xfce4 xfce4-terminal dbus-x11 \
-        xbase-clients xserver-xorg-video-dummy xvfb python3-packaging \
-        python3-psutil python3-xdg libgbm1 libutempter0 libfuse2 \
-        nload qbittorrent ffmpeg gpac fonts-lklug-sinhala tmate
-
-    echo "Installing Google Chrome..."
+    echo "Google Chrome Installing"
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    apt install -y ./google-chrome-stable_current_amd64.deb || apt --fix-broken install -y
+    dpkg --install google-chrome-stable_current_amd64.deb
+    apt install --assume-yes --fix-broken
+    
+    echo "Installing Firefox ESR"
+    add-apt-repository ppa:mozillateam/ppa -y  
+    apt update
+    apt install --assume-yes firefox-esr
+    apt install --assume-yes dbus-x11 dbus 
 
-    echo "Installing Chrome Remote Desktop..."
+    echo "Installing dependencies"
+    apt update
+    add-apt-repository universe -y
+    apt install --assume-yes xvfb xserver-xorg-video-dummy xbase-clients python3-packaging python3-psutil python3-xdg libgbm1 libutempter0 libfuse2 nload qbittorrent ffmpeg gpac fonts-lklug-sinhala
+    sudo apt update && sudo apt install -y tmate
+
+    echo "Installing Desktop Environment"
+    apt install --assume-yes xfce4 desktop-base xfce4-terminal xfce4-session
+    echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
+    apt remove --assume-yes gnome-terminal
+    apt install --assume-yes xscreensaver
+    systemctl disable lightdm.service
+
+    echo "Installing Chrome Remote Desktop"
     wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    apt install -y ./chrome-remote-desktop_current_amd64.deb || apt --fix-broken install -y
+    dpkg --install chrome-remote-desktop_current_amd64.deb
+    apt install --assume-yes --fix-broken
 
-    echo "Configuring CRD to use XFCE4..."
-    echo "exec /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
+    echo "Finalizing"
+    adduser "$username" chrome-remote-desktop
+    curl -s -L -k -o xfce-shapes.svg https://raw.githubusercontent.com/The-Disa1a/Cloud-Shell-GCRD/refs/heads/main/Wall/xfce-shapes.svg
+    sudo mv xfce-shapes.svg /usr/share/backgrounds/xfce/
+    echo "Wallpaper Changed"
+    
+    su - "$username" -c "$CRD --pin=$Pin"
+    service chrome-remote-desktop start
+    setup_storage "$username"
 
-    echo "Adding user to chrome-remote-desktop group..."
-    groupadd chrome-remote-desktop || true
-    usermod -aG chrome-remote-desktop "$username"
-
-    echo "Authenticating Chrome Remote Desktop..."
-    su - "$username" -c "/home/$username/crd-command.sh --pin=$Pin"
-
-    echo "CRD setup completed successfully!"
+    echo "RDP setup completed"
 }
 
 # Execute functions
 create_user
 setup_rdp
-setup_storage
 
 # Keep-alive loop
 echo "Starting keep-alive loop. Press Ctrl+C to stop."
 while true; do
     echo "I'm alive"
-    sleep 300
+    sleep 300  # Sleep for 5 minutes
 done
